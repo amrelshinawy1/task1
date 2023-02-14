b3f050bb9107bc63c413cb60f083988ddb740377
amrelshinawy1 <amrelshinawy1@gmail.com>
2023-02-15 00:51:56 +0200

    • Hard-code 5 senders and 10 bikers.

--- src/user/user-data-initializer.service.ts
+++ src/user/user-data-initializer.service.ts
@@ -21,12 +21,77 @@ export class UserDataInitializerService
       password: 'password',
       email: 'amr@example.com',
       roles: [RoleType.SENDER],
+    },{
+      username: 'amr1',
+      password: 'password',
+      email: 'amr1@example.com',
+      roles: [RoleType.SENDER],
+    },{
+      username: 'amr2',
+      password: 'password',
+      email: 'amr2@example.com',
+      roles: [RoleType.SENDER],
+    },{
+      username: 'amr3',
+      password: 'password',
+      email: 'amr3@example.com',
+      roles: [RoleType.SENDER],
+    },{
+      username: 'amr4',
+      password: 'password',
+      email: 'amr4@example.com',
+      roles: [RoleType.SENDER],
     }];
 
     const bikers = [{
-      username: 'admin',
+      username: 'admin1',
       password: 'password',
-      email: 'admin@example.com',
+      email: 'admin1@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin2',
+      password: 'password',
+      email: 'admin2@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin3',
+      password: 'password',
+      email: 'admin3@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin4',
+      password: 'password',
+      email: 'admin4@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin5',
+      password: 'password',
+      email: 'admin5@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin6',
+      password: 'password',
+      email: 'admin6@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin7',
+      password: 'password',
+      email: 'admin7@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin8',
+      password: 'password',
+      email: 'admin8@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin9',
+      password: 'password',
+      email: 'admin9@example.com',
+      roles: [RoleType.BIKER],
+    },{
+      username: 'admin10',
+      password: 'password',
+      email: 'admin10@example.com',
       roles: [RoleType.BIKER],
     }];
     await this.userModel.create(...senders, ...bikers).then(
